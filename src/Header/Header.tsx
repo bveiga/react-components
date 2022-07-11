c9d6452e5efc6a10f6c3e2431bba005380c180ae
Bruno Veiga <bspveiga@gmail.com>
2022-07-10 19:24:05 -0500

    Fixing spacing of nav items on mobile and adding new light vs dark option

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -10,46 +10,59 @@ interface HeaderProps {
 	// Navigation properties
 	navList?: LinkElement[];
 	activeIndex?: number;
+
+	// Styling
+	isLight?: Boolean;
 }
 
-const Header: FC<HeaderProps> = ({ brandName, logo, navList, activeIndex }) => {
+const Header: FC<HeaderProps> = ({
+	brandName,
+	logo,
+	navList,
+	activeIndex,
+	isLight = true
+}) => {
 	let navClasses = 'collapse navbar-collapse';
 	const [state, setState] = useState({
 		navToggled: false
 	});
 
+	let navbarShade: string = isLight ? 'light' : 'dark';
+
 	return (
 		// TODO: add back the 'fixed-top' class
-		<nav className='vs-header navbar navbar-expand-sm navbar-light'>
-			<div className='container'>
-				{/* Logo/Brand */}
-				<a className='vs-brand navbar-brand' href='/'>
-					{logo && <img src={logo} className='vs-brand__logo' alt='logo' />}
-					<span className={logo ? 'vs-brand__name sr-only' : 'vs-brand__name'}>{brandName}</span>
-				</a>
+		<header className="vs-header">
+			<nav className={'navbar navbar-expand-sm navbar-'+navbarShade}>
+				<div className='container'>
+					{/* Logo/Brand */}
+					<a className='vs-brand navbar-brand' href='/'>
+						{logo && <img src={logo} className='vs-brand__logo' alt='logo' />}
+						<span className={logo ? 'vs-brand__name sr-only' : 'vs-brand__name'}>{brandName}</span>
+					</a>
 
-				{/* Mobile Toggle */}
-				<button
-					className='navbar-toggler'
-					data-toggle='collapse'
-					data-target='#navbarNav'
-					aria-controls='navbarNav'
-					aria-expanded='false'
-					aria-label='Toggle navigation'
-					onClick={() => setState({ ...state, navToggled: !state.navToggled })}>
-					<span className='navbar-toggler-icon'></span>
-				</button>
+					{/* Mobile Toggle */}
+					<button
+						className='navbar-toggler'
+						data-toggle='collapse'
+						data-target='#navbarNav'
+						aria-controls='navbarNav'
+						aria-expanded='false'
+						aria-label='Toggle navigation'
+						onClick={() => setState({ ...state, navToggled: !state.navToggled })}>
+						<span className='navbar-toggler-icon'></span>
+					</button>
 
-				{/* Navigation */}
-				{navList && (
-					<div className={state.navToggled ? navClasses + ' show' : navClasses} id='navbarNav'>
-						<ul className='navbar-nav ml-auto'>
-							{navList.map((link, index) => renderNavigation(link, index, activeIndex))}
-						</ul>
-					</div>
-				)}
-			</div>
-		</nav>
+					{/* Navigation */}
+					{navList && (
+						<div className={state.navToggled ? navClasses + ' show' : navClasses} id='navbarNav'>
+							<ul className='navbar-nav ml-auto'>
+								{navList.map((link, index) => renderNavigation(link, index, activeIndex))}
+							</ul>
+						</div>
+					)}
+				</div>
+			</nav>
+		</header>
 	);
 }
 
